a10343183037b6cb2991bb24a675ebbfa2c53ec4
Hammerc124 <hammer.calebr@gmail.com>
2024-07-11 19:27:13 -0400

    adding all code to commit

--- server/schemas/index.js
+++ server/schemas/index.js
@@ -1,4 +1,25 @@
 const typeDefs = require('./typeDefs');
 const resolvers = require('./resolvers');
 
+let chatSchema = mongoose.Schema({
+    messages: [{
+        text: {
+            type: String,
+            max: 400,
+        },
+        sender: {
+            type: mongoose.Schema.Type.ObjectId,
+            ref: 'User'
+        }
+    }],
+    participant1: [{
+        type: mongoose.Schema.Type.ObjectId,
+        ref: 'User'
+    }],
+    participant2: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }]
+});
+
 module.exports = { typeDefs, resolvers };
