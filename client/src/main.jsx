7fa028e3255d8420a4e73a1f285606d3411441b1
Simplyareed <adam.reed1991@gmail.com>
2024-07-17 12:21:05 -0700

    added code inside of main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,25 @@
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Login from './Login.jsx'
+import NoMatch from './NoMatch.jsx'
 import App from './App.jsx'
 import './index.css'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NoMatch />,
+    children: [
+       {
+        path: '/login',
+        element: <Login />
+      },
+    ],
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
-)
+  <RouterProvider router={router} />
+);
