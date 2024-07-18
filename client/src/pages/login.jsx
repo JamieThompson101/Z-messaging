8bf32879a4d8a7429182611acd548fd63878972f
katzeundmais <zoellaphine@gmail.com>
2024-07-17 20:35:59 -0400

    i fixed the dashboard thing hehehe

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useMutation } from '@apollo/client';
-import { Link, navigate } from 'react-router-dom'; // Import the 'navigate' function
+import { Link, useNavigate } from 'react-router-dom'; // Import the 'navigate' function
 import { LOGIN } from '../utils/mutation';
 import Auth from '../utils/auth';
 import './loginsignup.css';
@@ -17,7 +17,7 @@ function Login(_props) {
             });
             const token = mutationResponse.data.login.token;
             Auth.login(token);
-            navigate('/dashboard'); // Redirect to dashboard on successful login
+            useNavigate('/dashboard'); // Redirect to dashboard on successful login
         } catch (e) {
             console.log(e);
         }
